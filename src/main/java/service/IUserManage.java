15b3b1c435104eeb888b641ab8da692fee90368a
PhillipsJackChen <188621723162@163.com>
2016-07-17 23:18:50 +0800

    src/

---
+++ src/main/java/service/IUserManage.java
@@ -0,0 +1,10 @@
+package service;
+
+import entity.User;
+
+
+public interface IUserManage {
+	public void addUser(User user);
+	public void modifyUser(User user);
+	public void deleteUser(User user);
+}
